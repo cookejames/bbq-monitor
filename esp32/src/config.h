35d6a7a6b831b924761d07d9db1c1027c9e66b07
James Cooke <james.cooke@bigpayme.com>
2021-04-22 12:33:01 +0100

    Add multiple board definitions

--- esp32/src/config.h
+++ esp32/src/config.h
@@ -3,18 +3,29 @@
 #include <ArduinoLog.h>
 #include <PID_v1.h>
 
+#define BOARD_DEVKIT
+// #define BOARD_TTGO_T5
+
 #define LOG_LEVEL LOG_LEVEL_TRACE
 
 #define STATUS_PIN 2
 
-#define SERVO_PIN 22 //26
+#ifdef BOARD_DEVKIT
+#define SERVO_PIN 26
+#define FAN_PWM_PIN 27
+#define FAN_RPM_PIN 13
+#endif
+
+#ifdef BOARD_TTGO_T5
+#define SERVO_PIN 22
+#define FAN_PWM_PIN 21
+#define FAN_RPM_PIN 13
+#endif
+
 #define SERVO_OPEN 180 // The angle the servo is open at
 #define SERVO_CLOSED 3 // The angle the servo is closed at
 #define SERVO_MIN_PULSE_WIDTH 1100
 #define SERVO_MAX_PULSE_WIDTH 2200
-
-#define FAN_PWM_PIN 21 //27
-#define FAN_RPM_PIN 13
 
 #define STARTUP_MODE_ENABLED false
 #define STARTUP_MODE_PERCENTAGE 0.7 // startup mode runs until this percentage of the setpoint
